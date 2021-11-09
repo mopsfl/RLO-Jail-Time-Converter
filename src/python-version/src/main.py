55cf1328f01131fe2d16074831a69403ce2f6f22
Flo <92175215+mopsfl@users.noreply.github.com>
2021-11-09 19:23:17 +0100

    Create main.py
    Added python version

---
+++ src/python-version/src/main.py
@@ -0,0 +1,24 @@
+import os
+from typing import Match
+
+print("mopsfl - Real Life Online - Jail Time Convert - v.0.1\n")
+
+try:
+    months = int(input("Enter the months: "))
+    method = input("Enter to what you want to convert it (hour, day): ")
+
+    
+    if method == "hour":
+        print("\nResults: " + str(months/60) + " hour(s)")
+        print("Results rounded: " + str(round(months/60)) + " hour(s)\n\n")
+    elif method == "day":
+        print("\nResults: " + str(months/1440) + " day(s)")
+        print("Results rounded: " + str(round(months/1440)) + " day(s)\n\n")
+    else:
+        print("Invalid method")
+
+    os.system('pause')
+except:
+    print("Invalid input")
+    os.system('pause')
+
